8cfc8bdf27b972253bf7dff24b61410c2d601c4a
raskovnik <4lch3my.3ck0@gmail.com>
2025-02-27 13:18:15 +0300

    refactor: move all models into one models file

--- internal/models/data_source.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type DataSource struct {
-	Id          int    `json:"id"`
-	Name        string `json:"string"`
-	Url         string `json:"url"`
-	Descriptoin string `json:"description"`
-}
--- internal/models/indicator.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Indicator struct {
-	Id          int    `json:"id"`
-	Name        string `json:"name"`
-	Category    string `json:"category"`
-	Unit        string `json:"unit"`
-	Description string `json:"description"`
-}
