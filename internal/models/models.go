a2edc76a81cd3697ffd47cc7e5d975de5467343a
Anthony Mwangi <4lch3my.3ck0@gmail.com>
2025-02-28 08:20:22 +0300

    feat: initial model design & project structure (#2)
    * initial model design & project structure
    
    * refactor: move all models into one models file
    
    * db: add initial migration file for all database tables

---
+++ internal/models/models.go
@@ -0,0 +1,49 @@
+package models
+
+import "time"
+
+type DataPoint struct {
+	Id          int       `json:"id"`
+	LocationId  int       `json:"location_id"`
+	IndicatorId int       `json:"indicator_id"`
+	SourceId    int       `json:"source_id"`
+	Date        time.Time `json:"date"`
+	Value       float64   `json:"value"`
+	IsEstimated bool      `json:"is_estimated"`
+}
+
+type DataSource struct {
+	Id          int    `json:"id"`
+	Name        string `json:"string"`
+	Url         string `json:"url"`
+	Descriptoin string `json:"description"`
+}
+
+type Indicator struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name"`
+	Category    string `json:"category"`
+	Unit        string `json:"unit"`
+	Description string `json:"description"`
+}
+
+type LocationRegion struct {
+	LocationId int       `json:"location_id"`
+	RegionId   int       `json:"region_id"`
+	StartDate  time.Time `json:"start_date"`
+	EndDate    time.Time `json:"end_date"`
+}
+
+type Location struct {
+	Id         int    `json:"id"`
+	Name       string `json:"name"`
+	AdminLevel int    `json:"admin_level"`
+	ParentId   int    `json:"parent_id"`
+	IsoCode    string `json:"iso_code"`
+	Map        string `json:"map"`
+}
+
+type Region struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
