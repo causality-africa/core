a4db9632c99fdee9790a51364c737e42355a35c5
Stephen Mwangi <mail@stephenmwangi.com>
2025-03-31 14:48:06 +0300

    fix: logging errors to sentry

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -7,6 +7,11 @@ import (
 	"core/internal/db"
 	"log/slog"
 	"os"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	sentryslog "github.com/getsentry/sentry-go/slog"
+	slogmulti "github.com/samber/slog-multi"
 )
 
 var (
@@ -14,17 +19,36 @@ var (
 )
 
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	slog.SetDefault(logger)
-
 	slog.Info("Starting Core...", "version", Version)
 
+	// Config
 	cfg, err := config.Load()
 	if err != nil {
 		slog.Error("Failed to load config", "error", err)
 		return
 	}
 
+	// Logging
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn:              cfg.Observability.SentryDSN,
+		EnableTracing:    true,
+		TracesSampleRate: 1.0,
+		SendDefaultPII:   false,
+	}); err != nil {
+		slog.Warn("Failed to initialized Sentry", "error", err)
+	}
+	defer sentry.Flush(2 * time.Second)
+
+	sentryHandler := sentryslog.Option{Level: slog.LevelWarn}.NewSentryHandler()
+	stdoutHandler := slog.NewTextHandler(
+		os.Stdout,
+		&slog.HandlerOptions{Level: slog.LevelDebug},
+	)
+	logger := slog.New(slogmulti.Fanout(stdoutHandler, sentryHandler))
+	logger = logger.With("release", Version)
+	slog.SetDefault(logger)
+
+	// Database
 	db, err := db.New(&cfg.DB)
 	if err != nil {
 		slog.Error("Failed to connect to DB", "error", err)
@@ -32,13 +56,15 @@ func main() {
 	}
 	defer db.Close()
 
+	// Cache
 	cache, err := cache.New(&cfg.Cache)
 	if err != nil {
 		slog.Error("Failed to connect to cache", "error", err)
 		return
 	}
 
-	server := api.New(db, cache, &cfg.Observability, Version)
+	// API
+	server := api.New(db, cache, Version)
 	err = server.Start(&cfg.API)
 	if err != nil {
 		slog.Error("Failed to start server", "error", err)
--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.24.0
 require (
 	github.com/getsentry/sentry-go v0.31.1
 	github.com/getsentry/sentry-go/echo v0.31.1
+	github.com/getsentry/sentry-go/slog v0.31.1
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/knadh/koanf/providers/env v1.0.0
 	github.com/knadh/koanf/v2 v2.1.2
 	github.com/labstack/echo/v4 v4.13.3
+	github.com/samber/slog-multi v1.4.0
 	github.com/valkey-io/valkey-go v1.0.56
 )
 
@@ -23,6 +25,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/samber/lo v1.49.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
